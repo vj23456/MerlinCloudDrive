a5a3e66bf3361e204a6e5c04b9d5b93b16fc2849
vj23456 <53201527+vj23456@users.noreply.github.com>
2024-10-12 18:09:59 +0800

    add v1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# CloudDrive
+
+## 是什么？
+`运行于ARM KoolCenter软件中心的，一个强大的多云盘管理工具，为用户提供包含云盘本地挂载的一站式的多云盘解决方案。`
+
+## 二进制项目地址
+https://www.clouddrive2.com/index.html
+
+
+## 机型支持
+
+在asuswrt为基础的固件上，插件目前支持HND和MTK和QCA架构的路由器
+
+
+---
---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "author": "vj23456", 
+    "description": "一个强大的多云盘管理工具，为用户提供包含云盘本地挂载的一站式的多云盘解决方案", 
+    "home_url": "Module_clouddrive.asp", 
+    "link": "https://github.com/vj23456/MerlinCloudDrive", 
+    "md5": "44ca9b43b762bb0b24b49d6c40fb044f", 
+    "module": "clouddrive", 
+    "tags": "工具", 
+    "title": "CloudDrive2", 
+    "version": "1.0"
+}
